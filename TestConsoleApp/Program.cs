e24727ce38d809c173406c83fc3729f917cc5e81
Михаил Казанник <kazannik.m@mail.ru>
2026-04-17 22:04:55 +0300

    fix: test app

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -11,10 +11,10 @@ namespace TestConsoleApp
 	{
 		static void Main(string[] args)
 		{
-			Uri uri  = new Uri(baseUri: new Uri("http://localhost:11434"), relativeUri: "/v1");
+			Uri uri = new Uri("http://localhost:11434");
 			try
 			{
-				IEnumerable<string> names = LLMConnectorLibrary.LLMOpenAI.GetModelsName(uri);
+				IEnumerable<string> names = LLMConnectorLibrary.LLMOpenAI.GetModelsName(uri, new TimeSpan(0,3,0));
 				foreach (string name in names)
 				{
 					Console.WriteLine(name);
